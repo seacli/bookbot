1e8f8d0aea18e446c67faaef96cf53224a9b42b4
seacli <oliver.tepper@kraken.tech>
2025-12-29 12:22:58 +0100

    Word counting

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+import string
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
+def count_words(content):
+    # clean = content.translate(str.maketrans('', '', string.punctuation))
+    # return len(clean.split())
+    return len(content.split())
+
 def main():
     text = get_book_text("books/frankenstein.txt")
     print(text)
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
+    
 
-main()
+if __name__ == "__main__":
+    main()
