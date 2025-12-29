ad59a1e4c9c711da4d5d3ece14102abac9f3fcb5
seacli <oliver.tepper@kraken.tech>
2025-12-29 11:24:07 +0100

    Read file

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    print(text)
+
+main()
